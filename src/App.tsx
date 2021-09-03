a5a3b9f750aa7db65b4deadb44e12ae9e293ceae
Kristjan Jansen <kristjan.jansen@gmail.com>
2021-09-03 22:15:21 +0300

    feat: tailwind (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1>Hello</h1>
-      <p>
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is: {count}
-        </button>
-      </p>
+    <div className="grid grid-cols-[64px,1fr] min-h-screen">
+      <div className="bg-gradient-to-b from-sky-500 to-sky-600 grid justify-center content-between py-5">
+        <img src="/logo-2020.svg" className="scale-100 w-10" />
+      </div>
+      <div className="p-3"></div>
     </div>
   );
 }
