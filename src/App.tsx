bbf2737afb388e91e6d28c17d328bb26d2716e16
kristjanjansen <kristjan.jansen@gmail.com>
2021-09-06 19:17:50 +0300

    reorg

--- src/App.tsx
+++ src/App.tsx
@@ -23,35 +23,34 @@ export function MdiLeaf(props: SVGProps<SVGSVGElement>) {
 export default function App() {
   return (
     <div className="grid grid-cols-[auto,1fr] min-h-screen">
-      <div className="w-16 bg-gradient-to-b from-sky-500 to-sky-600 grid justify-center content-start gap-4 py-8">
-        <Logo className="w-7 justify-self-center" />
-        <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
-          <Dashboard className="stroke-current w-6 text-white" />
-        </div>
-        <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
-          <Transports className="stroke-current w-6 text-white" />
-        </div>
-        <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
-          <Network className="stroke-current w-6 text-white" />
-        </div>
-        <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
-          <Places className="stroke-current w-6 text-white" />
-        </div>
-        <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
-          <Connected className="stroke-current w-6 text-white" />
-        </div>
-        <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
-          <Settings className="stroke-current w-6 text-white" />
-        </div>
-        <div className="group w-11 h-11 bg-white rounded-[42%] grid place-items-center transition-colors">
-          <Leaf className="stroke-current w-6 text-blue-500 group-hover:text-blue-600" />
+      <div className="w-16">
+        <div className="h-screen sticky top-0 bg-gradient-to-b from-sky-500 to-sky-600 grid justify-center content-start gap-4 py-8">
+          <Logo className="w-7 justify-self-center" />
+          <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
+            <Dashboard className="stroke-current w-6 text-white" />
+          </div>
+          <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
+            <Transports className="stroke-current w-6 text-white" />
+          </div>
+          <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
+            <Network className="stroke-current w-6 text-white" />
+          </div>
+          <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
+            <Places className="stroke-current w-6 text-white" />
+          </div>
+          <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
+            <Connected className="stroke-current w-6 text-white" />
+          </div>
+          <div className="w-11 h-11 hover:bg-sky-400 rounded-[42%] grid place-items-center transition-colors">
+            <Settings className="stroke-current w-6 text-white" />
+          </div>
+          <div className="group w-11 h-11 bg-white rounded-[42%] grid place-items-center transition-colors">
+            <Leaf className="stroke-current w-6 text-green-500 group-hover:text-blue-600" />
+          </div>
         </div>
       </div>
-      <div className="p-8">
-        <h1 className="p-0 m-0 text-6xl font-light font-title">
-          Real-time visibility
-        </h1>
-        <br />
+      <div className="p-8 grid gap-y-6">
+        <h1 className="text-6xl font-light font-title">Real-time visibility</h1>
         <p className="text-xl leading-8">
           When’s the next update? Right now. Know the location, ETA, and status
           of your shipments in real-time.&nbsp;Sixfold’s powerful AI
@@ -59,7 +58,6 @@ export default function App() {
           aggregating live data from supply chains across Europe on our powerful
           network.
         </p>
-        <br />
         <p>
           <a
             href=""
@@ -68,57 +66,53 @@ export default function App() {
             Request a demo
           </a>
         </p>
-        <br />
         <h3 className="flex items-center font-sans text-xl font-semibold text-sky-900">
           Higher customer satisfaction
         </h3>
-        <br />
         <p className="leading-6">
           Keep your customers informed where their order is and when it arrives.
           This will drive customer satisfaction and helps businesses to make
           customers happy.
         </p>
-        <br />
         <a
           href=""
           className="font-title text-2xl font-light leading-8 no-underline cursor-pointer hover:text-blue-500"
         >
           What’s the current state of supply chain sustainability?
         </a>
-        <br />
         <div className="font-title text-2xl leading-8 text-gray-900">
           Create places from the addresses in your transport data.
         </div>
-        <br />
         <div className="text-sm text-gray-900">
           Places help to improve real-time visibility and tracking accuracy of
           transports at your warehouses and customer locations.
         </div>
-        <br />
         <div className="text-sm text-gray-600">
           Only admins can create places.
         </div>
-        <br />
-        <button className="round inline-flex relative justify-center items-center py-2 px-4 text-sm font-medium text-center text-white align-middle whitespace-pre bg-blue-500 hover:bg-blue-600 transition-colors rounded border border-transparent border-solid cursor-pointer">
-          Create a saved filter
-        </button>
+        <div>
+          <button className="round inline-flex relative justify-center items-center py-2 px-4 text-sm font-medium text-center text-white align-middle whitespace-pre bg-blue-500 hover:bg-blue-600 transition-colors rounded border border-transparent border-solid cursor-pointer">
+            Create a saved filter
+          </button>
+        </div>
         <p className="py-4 px-3 my-4 mx-0 text-sm leading-6 text-blue-900 bg-orange-100 rounded border border-gray-300 border-solid shadow-sm">
           This carrier has set up its GPS (telematics provider) and is ready to
           send visibility data to you. Once they have allocated their vehicle to
           a transport, Sixfold will mark this carrier as fully connected.
         </p>
-        <br />
-        <div className="inline-block rounded-full py-1 px-2 text-xs font-semibold leading-4 text-green-700 bg-green-100 select-none shadow whitespace-no-wrap">
-          Ready for tracking
+        <div>
+          <div className="inline-block rounded-full py-1 px-2 text-xs font-semibold leading-4 text-green-700 bg-green-100 select-none shadow whitespace-no-wrap">
+            Ready for tracking
+          </div>
         </div>
-        <p />
-        <br />
-        <button
-          data-test-id="filterApplyButton"
-          className="inline-flex relative justify-center items-center py-1 px-2 m-0 text-sm font-medium leading-4 text-center text-white align-middle whitespace-pre bg-green-600 hover:bg-green-700 transition-colors rounded  cursor-pointer"
-        >
-          Apply
-        </button>
+        <div>
+          <button
+            data-test-id="filterApplyButton"
+            className="inline-flex relative justify-center items-center py-1 px-2 m-0 text-sm font-medium leading-4 text-center text-white align-middle whitespace-pre bg-green-600 hover:bg-green-700 transition-colors rounded  cursor-pointer"
+          >
+            Apply
+          </button>
+        </div>
       </div>
     </div>
   );
