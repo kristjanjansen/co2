7eb1757a5032e30f9465c2872e73e00fa40356bf
kristjanjansen <kristjan.jansen@gmail.com>
2021-09-09 19:10:11 +0300

    cleanup

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,10 +5,6 @@ import { LinkExternal } from "../components/LinkExternal";
 import { Suggestions } from "../components/Suggestions";
 import { Title } from "../components/Title";
 
-// VITE_BM_URL_MGMT=https://dashboard.bigmile.eu/single/?appid=70837c66-0ef1-4734-85f7-8808d28f7080&sheet=0[…]374-a96c-1df876a31f8f&opt=ctxmenu,currsel&identity=preview
-// VITE_BM_URL_EMISSIONS=https://dashboard.bigmile.eu/single/?appid=70837c66-0ef1-4734-85f7-8808d28f7080&sheet=6[…]8da-a846-ddab81beeeb9&opt=ctxmenu,currsel&identity=preview
-// VITE_BM_URL_MODE=https://dashboard.bigmile.eu/single/?appid=70837c66-0ef1-4734-85f7-8808d28f7080&sheet=c[…]ee8-8de5-5f9ca50e8009&opt=ctxmenu,currsel&identity=preview
-
 export function Index() {
   const links = [
     {
