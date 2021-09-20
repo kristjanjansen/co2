60bfd10e6189d157d4d96b6796f2f15e504e5bee
kristjanjansen <kristjan.jansen@gmail.com>
2021-09-20 19:36:40 +0300

    more cleanup

--- README.md
+++ README.md
@@ -33,3 +33,11 @@ Deploy to any provider that has a build step and environment variables support, 
 1. Set the environment variables above
 2. Set **build command** to `npm run build`
 3. Set **output directory** to `/dist`
+
+### Technical notes
+
+The prototype is implemented with React, React Router, Tailwind for CSS (with Sixfold branding) and Vite as a build tool.
+
+For tables there is a customized component on top of `@mui/x-data-grid`, the data is fetched for Google Sheets API using `use-google-sheets` hook.
+
+There are additional code snippets that did not ended up in the prototype, such as custom Tailwind-based [table component](https://gist.github.com/kristjanjansen/631ddbcc9f493233879aed3e53c8f594#file-transports-tsx) and Observable [Plot](https://observablehq.com/@observablehq/plot)-based [data visualization component](https://gist.github.com/kristjanjansen/631ddbcc9f493233879aed3e53c8f594#file-data-tsx).
--- src/pages/Lanes.tsx
+++ src/pages/Lanes.tsx
@@ -8,54 +8,6 @@ const headerClassName =
 const cellClassName =
   "whitespace-pre px-4 py-2 border-r border-gray-300 text-sm  cursor-pointer";
 
-/*
-const columns: GridColDef[] = [
-  {
-    field: "id",
-    headerName: "ID",
-    headerClassName,
-    cellClassName,
-    width: 200,
-  },
-  {
-    field: "firstName",
-    headerName: "First name",
-    headerClassName,
-    cellClassName,
-    width: 200,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    headerClassName,
-    cellClassName,
-    width: 200,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    headerClassName,
-    cellClassName,
-    renderCell: (cell) => (
-      <div className="bg-blue-500 w-full">{cell.value}</div>
-    ),
-    width: 200,
-  },
-];
-*/
-// const rows = [
-//   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-//   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-// ];
-
 export function Lanes() {
   const { data, loading, error } = useData(
     `${import.meta.env.VITE_CLIENT || ""}-lanes`
@@ -93,8 +45,6 @@ export function Lanes() {
         //   ),
       }))
     : [];
-
-  //console.log(columns2);
 
   if (loading) {
     return (
